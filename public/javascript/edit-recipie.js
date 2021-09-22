286c328186c71750ccd7d7bd393f4529675a8ccc
Lola <lia1m@aol.com>
2021-09-22 13:27:06 -0400

    editted css and button positioning

--- public/javascript/edit-recipie.js
+++ public/javascript/edit-recipie.js
@@ -1,25 +1,31 @@
 async function editFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="Title"]').value.trim();
-    const id = window.location.toString().split('/')
+  event.preventDefault();
 
-    const response = await fetch(`/api/recipies/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        title
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
   
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert(response.statusText);
+  const description = document.querySelector('textarea[name="Description"]').value.trim();
+  const ingredients = document.querySelector('textarea[name="Ingredients"]').value.trim();
+  const table_meals = document.querySelector('textarea[name="TableMeals"]').value.trim();
+  const directions = document.querySelector('textarea[name="Directions"]').value.trim();
+  const id = window.location.toString().slice(-1)
+  console.log(description);
+  const response = await fetch(`/api/recipie/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      description,
+      ingredients,
+      table_meals,
+      directions
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
+
+  if (response.ok) {
+    document.location.replace(`/dashboard/recipie/${id}`);
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('.edit-form').addEventListener('submit', editFormHandler);
-  
+}
+
+document.querySelector('#edit-form').addEventListener('submit', editFormHandler);
